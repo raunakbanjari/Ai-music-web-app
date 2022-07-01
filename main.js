b362c34ddf1c85d21c01000dd7c3fa66a8e36a66
RAUNAK BANJARI <88834316+raunakbanjari@users.noreply.github.com>
2022-07-01 18:32:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,62 +1,93 @@
-happy = "";
-harry = "";
-
-rightWristX = 0;
-rightWristY = 0;
+var song = "";
 leftWristX = 0;
+RightWristX = 0;
+RightWristY = 0;
 leftWristY = 0;
-scoreleftWrist = 0;
-harry_song = "";
-
-function preload(){
-    happy = loadSound("happy.mp3");
-    harry = loadSound("harry.mp3");
-}
+scoreleftwrist = 0;
+scoreRightwrist = 0;
 
 function setup(){
-    canvas = createCanvas(400,400);
+    canvas = createCanvas(400 , 400);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
 
     posenet = ml5.poseNet(video , modelLoaded);
-    posenet.on('pose', gotPoses);
+    posenet.on('pose' , gotPoses);
 }
-
+function preload(){
+    song = loadSound("music.mp3");
+}
 function draw(){
-    image(video,0,0,400,400);
-    fill('red');
-    stroke('red');
-    harry_song = harry.isPlaying();
-    console.log(harry_song);
-    if(scoreleftWrist > 0.2){
-        circle(leftWristX -170 , leftWristY-60 , 20);
-        happy.stop();
-        if(harry_song == false){
-            harry.play();
-            console.log("playing harry potter theme song");
-            document.getElementById("song").innerHTML = "SONG NAME : HARRY POTTER THEME SONG";
-        }
-        
-    }
+ image(video, 0 , 0 , 400 , 400 );
 
+ fill("#FF0000");
+ stroke("#FF0000");
 
+ if(scoreleftwrist > 0.2){
+
+ 
+ 
+ circle(leftWristX , leftWristY , 20);
+ Innumberleftwrist = Number(leftWristY);
+ remove_decimals_of_left_wrist = floor(Innumberleftwrist);
+ volume = remove_decimals_of_left_wrist/500;
+ console.log("volume = " + volume);
+ document.getElementById("volume").innerHTML = "VOLUME = " + volume;
+ song.setVolume(volume);
+ }
+ if(scoreRightwrist > 0.2){
+//for drawing a circle to identify right wrist
+ circle(RightWristX , RightWristY , 20);
+ //for setting the speed of the song
+ if(RightWristY>0 && RightWristY<=100){
+    song.rate(0.5);
+    document.getElementById("speed").innerHTML = "SPEED = 0.5x";
+ }
+ else if(RightWristY>100 && RightWristY<=200){
+    song.rate(1);
+    document.getElementById("speed").innerHTML = "SPEED = 1x";
+ }
+ else if(RightWristY>200 && RightWristY<=300){
+    song.rate(1.5);
+    document.getElementById("speed").innerHTML = "SPEED = 1.5x";
+
+ }
+ else if(RightWristY>300 && RightWristY<=400){
+    song.rate(2);
+    document.getElementById("speed").innerHTML = "SPEED = 2x";
+ }
+ else if(RightWristY>400 && RightWristY<=500){
+    song.rate(2.5);
+    document.getElementById("speed").innerHTML = "SPEED = 2.5x";
+ }
+ }
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+    document.getElementById("speed").innerHTML = "SPEED = 1x";
 }
 function modelLoaded(){
-    console.log("POSENET IS INITIALIZED");
+    console.log("posenet is initialized");
 }
-
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
+
+        scoreleftwrist = results[0].pose.keypoints[9].score;
+        scoreRightwrist = results[0].pose.keypoints[10].score;
+        console.log(" score of the left wrist = " + scoreleftwrist);
+        console.log(" score of the right wrist = " + scoreRightwrist);
+
         leftWristX = results[0].pose.leftWrist.x;
+        RightWristX = results[0].pose.rightWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log("x coordinate of right wrist : " + rightWristX + " y coordinate of right wrist : " + rightWristY);
-        console.log("x coordinate of left wrist : " + leftWristX + " y coordinate of left wrist : " + leftWristX);
-        scoreleftWrist = results[0].pose.keypoints[9].score;
-        console.log(scoreleftWrist);
+        RightWristY = results[0].pose.rightWrist.y;
+        console.log("X coordinate of left wrist : " + leftWristX + "y coordinate of left wrist" + leftWristY);
+        console.log("X coordinate of right wrist : " + RightWristX + "y coordinate of right wrist" + RightWristY); 
+        
     }
 }
